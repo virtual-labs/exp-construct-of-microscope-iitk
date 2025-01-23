e70d4177713d14bf4ead6199f0d09a5982eb20da
vleadadmin <admin@vlabs.ac.in>
2025-01-23 04:58:04 +0000

    https://virtual-labs.github.io/virtual-labs/exp-construct-of-microscope-iitk click on the link to test your code.

---
+++ simulation/javascript/quiz.js
@@ -0,0 +1,116 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'Which is the part of the microscope that holds the objective lenses?',
+    answers: [
+      { text: 'Eye-Piece', correct: false },
+      { text: 'Nose-piece', correct: true },
+      { text: 'Mirror', correct: false },
+      { text: 'Condenser', correct: false }
+    ]
+  },
+  {
+    question: 'Light can be focused by adjusting the ____?',
+    answers: [
+      { text: 'Base', correct: false },
+      { text: 'Stage', correct:false  },
+      { text: 'Automatic Stop', correct: false },
+      { text: 'Condenser', correct: true }
+    ]
+  },
+  {
+    question: 'In an optical microscope, which of the following lens are convex in nature?',
+    answers: [
+      { text: 'Eye-piece', correct: false },
+      { text: 'Objective lenses', correct: false },
+      { text: 'Both a and b', correct: true },
+      { text: 'None of these', correct: false }
+    ]
+  },
+  {
+    question: 'Which part is used to hold the slides with the sample in an optical microscope?',
+    answers: [
+      { text: 'Stage and Clips.', correct: true },
+      { text: 'Arm', correct: false },
+      { text: 'Tube', correct: false },
+      { text: 'Knobe', correct: false }
+    ]
+  }
+]
